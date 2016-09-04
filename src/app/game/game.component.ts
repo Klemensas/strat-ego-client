4391f972839e72ab44e327c293d2b6e7630973eb
Klemensas <kdranseika@gmail.com>
2016-09-04 23:47:53 +0300

    Use socket service in game component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { GameDataService } from '../services/game-data.service';
+import { SocketService } from '../services/socket.service';
 
 @Component({
   moduleId: module.id,
@@ -11,15 +12,19 @@ import { GameDataService } from '../services/game-data.service';
 })
 export class GameComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private gameData: GameDataService) { }
+  constructor(private route: ActivatedRoute, private socket: SocketService, private gameData: GameDataService) { }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      let name = params['name'];
-      this.gameData.getWorldData(name).subscribe(d => {
-        console.log('hi', d)
-      });
-    });
+    console.log(this.route.data)
+    this.route.data.subscribe(val => {
+      console.log(val);
+    })
+    // this.route.params.subscribe(params => {
+    //   let name = params['name'];
+    //   this.gameData.getWorldData(name).subscribe(d => {
+    //     console.log('hi', d)
+    //   });
+    // });
   }
 
 }
