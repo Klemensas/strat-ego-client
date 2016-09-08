af3fcb52222f7b3c0e325053b1362fb3822f2983
Klemensas <kdranseika@gmail.com>
2016-09-08 23:47:38 +0300

    Use resouces component

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ResourcesComponent } from '../resources';
+
 @Component({
   moduleId: module.id,
   selector: 'restaurant',
   templateUrl: 'restaurant.component.html',
   styleUrls: ['restaurant.component.css'],
-  inputs: ['restData']
+  inputs: ['restData'],
+  directives: [ResourcesComponent]
 })
 export class RestaurantComponent implements OnInit {
+  restData;
+  resources = {};
+
   constructor() {
-    console.log(this['restData'])
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.manageResources(this.restData.resources);
+  }
+
+  manageResources(res) {
+    Object.keys(res).forEach(r => {
+      this.resources[r] = ++res[r];
+    });
+
+    setTimeout(() => this.manageResources(res), 1000);
+  }
 
 }
