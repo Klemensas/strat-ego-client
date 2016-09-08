4ca60b6f785d55729d2ab00ea35f591009bdc997
Klemensas <kdranseika@gmail.com>
2016-09-08 23:47:09 +0300

    Add map component

---
+++ src/app/resources/resources.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'resources',
+  templateUrl: 'resources.component.html',
+  styleUrls: ['resources.component.css'],
+  inputs: ['res']
+})
+export class ResourcesComponent implements OnInit {
+  res;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.res);
+  }
+
+}
