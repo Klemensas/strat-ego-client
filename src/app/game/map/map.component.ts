c5451e3766ba957334b1879adc4e120d8230299f
Klemensas <kdranseika@gmail.com>
2016-09-11 23:54:19 +0300

    Use map directive in map component

--- src/app/game/map/map.component.ts
+++ src/app/game/map/map.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Map } from '../directives/map.directive';
 
 @Component({
   moduleId: module.id,
   selector: 'map',
   templateUrl: 'map.component.html',
-  styleUrls: ['map.component.css']
+  styleUrls: ['map.component.css'],
+  directives: [Map]
 })
 export class MapComponent implements OnInit {
 
