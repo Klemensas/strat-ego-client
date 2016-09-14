2071c6a948cfc7708c0d073494544bc8167b3131
Klemensas <kdranseika@gmail.com>
2016-09-14 23:38:43 +0300

    Move map details from directive to component

--- src/app/game/map/map.component.spec.ts
+++ src/app/game/map/map.component.spec.ts
@@ -1,13 +1,13 @@
-/* tslint:disable:no-unused-variable */
+// /* tslint:disable:no-unused-variable */
 
-import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-import { addProviders, async, inject } from '@angular/core/testing';
-import { MapComponent } from './map.component';
+// import { By }           from '@angular/platform-browser';
+// import { DebugElement } from '@angular/core';
+// import { addProviders, async, inject } from '@angular/core/testing';
+// import { MapComponent } from './map.component';
 
-describe('Component: Map', () => {
-  it('should create an instance', () => {
-    let component = new MapComponent();
-    expect(component).toBeTruthy();
-  });
-});
+// describe('Component: Map', () => {
+//   it('should create an instance', () => {
+//     let component = new MapComponent();
+//     expect(component).toBeTruthy();
+//   });
+// });
