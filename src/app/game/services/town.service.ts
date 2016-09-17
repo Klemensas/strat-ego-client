f79d5f3249da785d4072e75a0464f7c56a4f66a9
Klemensas <kdranseika@gmail.com>
2016-09-17 18:12:28 +0300

    Refactor town service; add resource calculation, name changing

--- src/app/game/services/town.service.ts
+++ src/app/game/services/town.service.ts
@@ -1,25 +1,50 @@
 import { Injectable } from '@angular/core';
 
 import { SocketService } from './socket.service';
+import { PlayerService } from './player.service';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { Town } from '../models/Town';
 
 @Injectable()
 export class TownService {
-  public data = {};
-  public activeTown = null;
+  public data = new BehaviorSubject(<Town>{});
 
-  constructor(private socket: SocketService) {
+  constructor(private socket: SocketService, private playerService: PlayerService) {
+    this.playerService.activeTown.subscribe((town: Town) => {
+      this.data.next(town);
+    })
     this.observeTown();
+    this.calculateRes();
   }
 
   observeTown() {
     this.socket.events.town.subscribe(event => {
-      this.data = event.data;
-
-      console.log('data yo', this.data);
+      this.data.next(event);
+      console.log('ok?', event)
       // if (!this.activeRest && this.data.Restaurants.length === 1) {
       //   this.activeRest = this.data.Restaurants[0];
       // }
     })
+  }
+
+  calculateRes() {
+    if (this.data.value['resources']) {
+      const now = Date.now();
+      const time = new Date(this.data.value['updatedAt']).getTime();
+      const timePast = (now - time) / 1000 / 60;
+      const res = this.data.value['resources'];
+
+      res.clay += Math.floor(timePast);
+      res.wood += Math.floor(timePast);
+      res.iron += Math.floor(timePast);
+    }
+    setTimeout(() => this.calculateRes(), 1000);
+  }
+
+  changeName(name) {
+    this.socket.changeTownName(this.data.value._id, name)
   }
 
 }
