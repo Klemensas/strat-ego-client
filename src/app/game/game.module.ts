52db9a653fbad40483f2e010dc7e12fab97d7f8b
Klemensas <kdranseika@gmail.com>
2016-09-17 18:09:37 +0300

    Import forms to game module

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { FormsModule }    from '@angular/forms';
+import { FormsModule }    from '@angular/forms';
 
 import { PlayerService, TownService, SocketService } from './services/index';
 
@@ -12,7 +12,7 @@ import { WorldResolver } from './services/world.resolver';
 @NgModule({
   imports: [
     CommonModule,
-    // FormsModule,
+    FormsModule,
     gameRouting
   ],
   declarations: [
