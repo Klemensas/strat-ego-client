368b43536e5b0da83ce244c05705046c13813216
Klemensas <kdranseika@gmail.com>
2016-09-18 13:31:08 +0300

    Add building mapping to game-data service, save activeWorld

--- src/app/services/game-data.service.ts
+++ src/app/services/game-data.service.ts
@@ -4,13 +4,15 @@ import { AuthHttp, JwtHelper } from 'angular2-jwt';
 import { API, AUTH } from '../config';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/cache';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 // TODO: split this service into world and player services
 
 @Injectable()
 export class GameDataService {
   public data = {
-    world: this.getActiveWorlds()
+    world: this.getActiveWorlds(),
+    activeWorld: new BehaviorSubject(null)
   };
 
   constructor(private http:Http, private authHttp:AuthHttp) {}
@@ -18,6 +20,7 @@ export class GameDataService {
   getActiveWorlds() {
     return this.http.get(`${API}world`)
       .map(t => t.json())
+      .map(this.mapBuildings)
       .cache();
       // .flatMap(t => {
       //   localStorage.setItem('jwt', t);
@@ -26,26 +29,38 @@ export class GameDataService {
       // });
   }
 
-  getWorldData(target) {
-    return this.authHttp.get(`${API}world/${target}`)
-      .map(t => t.json())
-      .cache();
-      // .flatMap(t => {
-      //   localStorage.setItem('jwt', t);
-      //   this.token = t;
-      //   return this.getUser();
-      // });
+  mapBuildings(worlds) {
+    const data = worlds.map(world => {
+      const buildings = {};
+      world.buildingData.forEach(build => {
+        buildings[build.name] = build;
+      })
+      world.buildingData = buildings;
+      return world;
+    });
+    return data;
   }
 
-  joinWorld(target) {
-    return this.authHttp.get(`${API}world/${target}/join`)
-      .map(t => t.json())
-      .cache();
-  }
+  // getWorldData(target) {
+  //   return this.authHttp.get(`${API}world/${target}`)
+  //     .map(t => t.json())
+  //     .cache();
+  //     // .flatMap(t => {
+  //     //   localStorage.setItem('jwt', t);
+  //     //   this.token = t;
+  //     //   return this.getUser();
+  //     // });
+  // }
 
-  getPlayerData(target = this.authHttp) {
-    return this.authHttp.get(`${API}world/${target}/player`)
-      .map(t => t.json())
-      .cache();
-  }
+  // joinWorld(target) {
+  //   return this.authHttp.get(`${API}world/${target}/join`)
+  //     .map(t => t.json())
+  //     .cache();
+  // }
+
+  // getPlayerData(target = this.authHttp) {
+  //   return this.authHttp.get(`${API}world/${target}/player`)
+  //     .map(t => t.json())
+  //     .cache();
+  // }
 }
