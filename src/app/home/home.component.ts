54eea9869f8b55ecb2e54371931df250d38ad687
Klemensas <kdranseika@gmail.com>
2016-09-21 21:48:16 +0300

    Refactor home component to subscribe in constructor instead init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { GameDataService } from '../services/game-data.service';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'home',
@@ -11,18 +12,24 @@ import { Observable } from 'rxjs/Observable';
 export class HomeComponent implements OnInit {
   private worlds;
   private user;
-  constructor(private authService:AuthService, private gdService:GameDataService) {}
+  private userSubScription: Subscription;
 
-  ngOnInit() {
-    this.gdService.data.world
-      .subscribe(worlds => {
-        this.worlds = worlds;
-      });
-    this.authService.user.subscribe(
+  constructor(private authService:AuthService, private gdService:GameDataService) {
+    this.userSubScription = this.authService.user.subscribe(
       user => {
         this.user = user;
       }
     );
+    this.gdService.data.world
+      .subscribe(worlds => {
+        this.worlds = worlds;
+      });
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
   }
 
   userOnWorld(world) {
@@ -30,5 +37,3 @@ export class HomeComponent implements OnInit {
   }
 
 }
-
-
