ed56bf6130f3812c92b96517cb666af1120ead0c
Klemensas <kdranseika@gmail.com>
2016-10-02 23:35:43 +0300

    Add units component

---
+++ src/app/game/units/units.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { GameDataService } from '../../services/game-data.service'
+import { TownService } from '../services/town.service'
+
+@Component({
+  selector: 'units',
+  templateUrl: './units.component.html',
+  styleUrls: ['./units.component.scss']
+})
+export class UnitsComponent implements OnInit {
+  units;
+  constructor(private townService: TownService, private gameData: GameDataService) { }
+
+  ngOnInit() {
+    // this.gameData.data.activeWorld.subscribe(world => {
+    //   this.buildingData = world.buildingData;
+    //   this.buildingDataMap = world.buildingDataMap;
+    // });
+    this.townService.currentTown.subscribe(town => {
+      this.units = this.unitArray(town.units);
+    });
+  }
+
+  unitArray(units) {
+    const list = Object.keys(units);
+    return list.reduce((array, item) => {
+      const unit = units[item];
+      if (unit.amount || unit.queued) {
+        array.push({
+          name: item,
+          amount: unit.amount,
+          queued: unit.queued
+        });
+      }
+      return array;
+    }, [])
+  }
+
+}
