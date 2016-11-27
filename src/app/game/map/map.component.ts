aead9d023d7677c3374ad3883f58b23115d2c63f
Klemensas <kdranseika@gmail.com>
2016-11-27 16:42:22 +0200

    Use big lib for map math

--- src/app/game/map/map.component.ts
+++ src/app/game/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, AfterViewChecked , ElementRef, Renderer, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 import { MapService, PlayerService } from '../services';
+import { Observable } from 'rxjs';
+import * as cloneDeep from 'lodash/cloneDeep';
+import * as Big from 'big.js';
 
 @Component({
   selector: 'map',
@@ -8,28 +11,29 @@ import { MapService, PlayerService } from '../services';
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MapComponent implements OnInit, AfterViewChecked  {
-  private stage;
-  private layer;
+  public dragging = false;
 
+  private drawCoords = true;
   private images;
 
   private mapData;
   private mapOffset;
-  private markedTowns = {};
-  private isReady = false;
   private mapSettings = {
     size: { x: 0, y: 0 },
-    width: 0,
-    height: 0,
-    side: 0,
-    radius: 0,
-    aHeight: 0,
-    hexHeight: 0,
+    width: <any>0,
+    height: <any>0,
+    side: <any>0,
+    radius: <any>0,
+    aHeight: <any>0,
+    hexHeight: <any>0,
+    edgeGradient: <any>0,
     points: [],
     shouldDraw: false,
   };
   private ctx;
   private rng;
+  private drag;
+  // private dragging;
 
   @ViewChild('map') map;
 
@@ -50,23 +54,21 @@ export class MapComponent implements OnInit, AfterViewChecked  {
     });
 
     this.playerService.activeTown.subscribe(data => {
-        if (!data) {
-          console.error('No active town?', data, this.playerService);
-          return;
-        }
-        this.markedTowns[`${data.location[0]},${data.location[1]}`] = 'rgba(255,225,53, 0.4)';
-        this.mapOffset = this.centerOffset({ x: data.location[0], y: data.location[1] });
-        this.mapService.getMapData(location).then(mapData => {
-          this.mapData = mapData;
-          this.mapSettings.shouldDraw = true;
-            console.log(this.mapSettings.shouldDraw, this, this.mapOffset);
-        });
+      if (!data) {
+        console.error('No active town?', data, this.playerService);
+        return;
+      }
+      this.mapOffset = this.centerOffset({ x: data.location[0], y: data.location[1] });
+      this.mapService.getMapData(location).then(mapData => {
+        this.mapData = mapData;
+        this.mapSettings.shouldDraw = true;
+        console.log('got da data', this.mapSettings.shouldDraw, this, this.mapOffset, this.mapSettings.width);
+      });
     });
   }
 
   ngAfterViewChecked() {
     if (this.ctx && this.mapSettings.shouldDraw && this.mapData) {
-
       this.drawMap(this.mapOffset);
     }
   }
@@ -76,6 +78,7 @@ export class MapComponent implements OnInit, AfterViewChecked  {
       x: this.map.nativeElement.offsetWidth,
       y: this.map.nativeElement.offsetHeight,
     });
+    // this.mapOffset = this.centerOffset({})
     this.mapSettings.shouldDraw = true;
   }
 
@@ -83,67 +86,38 @@ export class MapComponent implements OnInit, AfterViewChecked  {
     console.log('click')
   }
 
-  public dragging(drag, event) {
-    console.log('toggle drag',drag)
-    if (drag) {
-      this.map.nativeElement.addEventListener('mousemove', this.mapDrag);
-    } else {
-      this.map.nativeElement.removeEventListener('mousemove', this.mapDrag);
-    }
+  public startDrag(event) {
+    const origin = { x: event.offsetX, y: event.offsetY };
+    const initialOffset = cloneDeep(this.mapOffset);
+    this.dragging = true;
+    Observable.fromEvent(this.map.nativeElement, 'mousemove')
+      .takeWhile(() => this.dragging)
+      .throttleTime(10)
+      .subscribe(data => this.mapDrag(origin, data, initialOffset))
   }
 
-  public mapDrag(event){
-
-  }
-  
-  private coordToPixel(location) {
-    const x = location.x;
-    const y = location.y;
-    const xOffset = y % 2 ? -0.5 : 0;
-    return {
-      x: (x + xOffset) * this.mapSettings.width,
-      y: (y * 0.75 - 0.25) * this.mapSettings.height
+  public mapDrag(origin, event, offset) {
+    const difference = {
+      x: origin.x - event.offsetX,
+      y: origin.y - event.offsetY
     };
-  }
 
-  private pixelToCoord(pos) {
-    const secX = Math.floor(pos.x / this.mapSettings.width);
-    const secY = Math.floor(pos.y / (this.mapSettings.hexHeight + this.mapSettings.side));
-    const secPxX = pos.x % this.mapSettings.width;
-    const secPxY = pos.y % (this.mapSettings.hexHeight + this.mapSettings.side);
-    let res = { x: secX, y: secY };
-    const m = this.mapSettings.hexHeight / this.mapSettings.radius;
-    if (secY % 2) { // B
-      if (secPxX >= this.mapSettings.radius) {
-        if (secPxY < (this.mapSettings.side - secPxX * m)) {
-          res.y = secY - 1;
-        }
-      } else {
-        if (secPxY < (secPxX * m)) {
-          res.y = secY - 1;
-        } else {
-          res.x = secX - 1;
-        }
-      }
-    } else {
-      if (secPxY < (this.mapSettings.hexHeight - secPxX * m)) {
-        res.x = secX - 1;
-        res.y = secY - 1;
-      }
-      if (secPxY < (-this.mapSettings.hexHeight + secPxX * m)) {
-        res.y = secY - 1;
-      }
-    }
-    return res;
+    if (Math.abs(difference.x) < 5 && Math.abs(difference.y) < 5) { return; }
+
+    this.mapOffset = this.mapService.pixelToCoord(
+      { x: offset.xPx.plus(difference.x), y: offset.yPx.plus(difference.y) },
+      this.mapSettings
+    );
+    this.mapSettings.shouldDraw = true;
   }
 
   private setMapSettings(size) {
-    const width = size.x / 13;
-    const side = width / Math.sqrt(3);
-    const radius = width / 2;
-    const height = side * 2;
-    const hexHeight = side / 2;
-    const aHeight = height * 0.75;
+    const width = new Big(size.x).div(13);
+    const side = width.div(Big(3).sqrt());
+    const radius = width.div(2);
+    const height = side.times(2);
+    const hexHeight = side.div(2);
+    const aHeight = height.times(0.75);
 
     this.mapSettings = {
       size,
@@ -153,93 +127,85 @@ export class MapComponent implements OnInit, AfterViewChecked  {
       height,
       aHeight,
       hexHeight,
+      edgeGradient: hexHeight.div(radius),
       shouldDraw: false,
       points: [
         { x: width, y: hexHeight },
         { x: width, y: aHeight },
         { x: radius, y: height },
-        { x: 0, y: aHeight },
-        { x: 0, y: hexHeight },
+        { x: Big(0), y: aHeight },
+        { x: Big(0), y: hexHeight },
       ]
     };
   }
 
   private centerOffset(location) {
+    const centerLoc = this.mapService.coordToPixel(location, this.mapSettings);
     const center = [
       this.mapSettings.size.x / 2,
       this.mapSettings.size.y / 2
     ];
-    const hexEnd = [
-      center[0] - this.mapSettings.width / 2,
-      center[1] - this.mapSettings.height / 2
-    ];
-    const yDiff = Math.ceil(hexEnd[1] / this.mapSettings.aHeight);
-    const yCoord = location.y - yDiff;
-    const parityChange = yDiff % 2;
-    const y = hexEnd[1] - yDiff * this.mapSettings.aHeight;
-    const xDiff = Math.ceil(hexEnd[0] / this.mapSettings.width + parityChange);
-    const xCoord = location.x - xDiff;
-    const xChange = parityChange ? (yCoord % 2 ? -this.mapSettings.width / 2 : this.mapSettings.width / 2) : 0;
-    const x = hexEnd[0] - xDiff * this.mapSettings.width + xChange;
-    const pixelLoc = this.coordToPixel(location);
-    const position = {
-      x: pixelLoc.x - center[0] - Math.abs(x),
-      y: pixelLoc.y - center[1] - Math.abs(y),
-    }
-    return { x, y, xCoord, yCoord, position };
+    const leftCorner = {
+      x: centerLoc.x.minus(center[0]),
+      y: centerLoc.y.minus(center[1]),
+    };
+    return this.mapService.pixelToCoord(leftCorner, this.mapSettings);
   }
 
   private drawMap(offset) {
-    const drawCoords = true;
-    const parity = offset.yCoord % 2;
-    const yMax = Math.ceil((this.mapSettings.size.y - offset.y) / this.mapSettings.aHeight);
+    const parity = +offset.yCoord % 2;
+    const yMax = +Big(this.mapSettings.size.y).minus(offset.y).div(this.mapSettings.aHeight).round(0, 3);
     // console.log(`Drawin y for ${yMax} rows`);
+    this.ctx.clearRect(0, 0, this.mapSettings.size.x, this.mapSettings.size.y);
     for (let y = 0; y < yMax; y++) {
-      const yCoord = offset.yCoord + y;
-      const yPos = offset.y + y * this.mapSettings.aHeight;
+      const yCoord = offset.yCoord.plus(y);
+      const yPos = Big(y).times(this.mapSettings.aHeight).plus(offset.y);
       let xOffset = offset.x;
       let x = 0;
-      if (y && yCoord % 2 !== parity) {
-        xOffset += parity ? this.mapSettings.width / 2 : -this.mapSettings.width / 2;
-        x -= parity ? 1 : 0;
+      if (y && +yCoord % 2 !== parity) {
+        const offsetModifier = parity ? 1: -1;
+        xOffset = xOffset.plus(this.mapSettings.width.times(offsetModifier).div(2));
+        x -= parity;
       }
-      const xMax = Math.ceil((this.mapSettings.size.x - offset.x - xOffset) / this.mapSettings.width);
+      const xMax = +Big(this.mapSettings.size.x).minus(offset.x).minus(xOffset).div(this.mapSettings.width);
       // console.log(`Drawin x for ${xMax} columns, when y is ${yCoord}`);
       for (; x < xMax; x++) {
-        const xCoord = offset.xCoord + x;
-        const xPos = x * this.mapSettings.width + xOffset;
-        const image = this.mapData[`${xCoord},${yCoord}`] ?
-          this.images[this.images.length - 1] :
-          this.images[Math.round(this.rng() * (this.images.length - 2))];
-
-        this.drawHex(xPos, yPos, image, this.markedTowns[`${xCoord},${yCoord}`]);
-        if (drawCoords) {
-          this.ctx.font = "20pt Calibri";
-          this.ctx.fillStyle = "#ff0000";
-          this.ctx.lineWidth = 1;
-          this.ctx.strokeText(`${xCoord};${yCoord}`, xPos, yPos + this.mapSettings.height / 2);
-          this.ctx.fillText(`${xCoord};${yCoord}`, xPos, yPos + this.mapSettings.height / 2);
-        }
+        const xCoord = offset.xCoord.plus(x);
+        const xPos = Big(x).times(this.mapSettings.width).plus(xOffset);
+        this.drawHex(xPos, yPos, `${xCoord},${yCoord}`);
       }
     }
     this.mapSettings.shouldDraw = false;
   };
 
-  private drawHex(x, y, image, fill) {
-      this.ctx.save();
-      this.ctx.beginPath();
-      this.ctx.moveTo(x + this.mapSettings.width / 2, y);
-      this.mapSettings.points.forEach(point => this.ctx.lineTo(point.x + x, point.y + y));
-      this.ctx.closePath();
-      this.ctx.clip();
-      this.ctx.drawImage(image, x, y, this.mapSettings.width, this.mapSettings.height);
-      this.ctx.restore();
-      this.ctx.stroke();
-      if (fill) {
-        this.ctx.globalCompositeOperation = 'overlay';
-        this.ctx.fillStyle = fill;
-        this.ctx.fill();
-        this.ctx.globalCompositeOperation = 'source-over';
-      }
+  private drawHex(x, y, coordString) {
+    const data = this.mapData[coordString];
+    const image = data ? this.images[this.images.length - 1] :
+      this.images[Math.round(this.rng(coordString) * (this.images.length - 2))];
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.moveTo(+x.plus(this.mapSettings.width.div(2)), +y);
+    this.mapSettings.points.forEach(point => {
+      this.ctx.lineTo(+point.x.plus(x), +point.y.plus(y));
+    });
+    this.ctx.closePath();
+    this.ctx.clip();
+    this.ctx.drawImage(image, +x, +y, +this.mapSettings.width, +this.mapSettings.height);
+    this.ctx.restore();
+    this.ctx.stroke();
+
+    if (data) {
+      this.ctx.globalCompositeOperation = 'darker';
+      this.ctx.fillStyle = data.owner ? 'yellow': 'rgba(100,100,100,0.4)';
+      this.ctx.fill();
+      this.ctx.globalCompositeOperation = 'source-over';
+    }
+    if (this.drawCoords) {
+      this.ctx.font = "20pt Calibri";
+      this.ctx.fillStyle = "#ff0000";
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeText(coordString, +x, +y + this.mapSettings.height / 2);
+      this.ctx.fillText(coordString, +x, +y + this.mapSettings.height / 2);
+    }
   }
 }
