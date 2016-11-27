eccc95837658b58a4aeba460c8668fbdf2c24c61
Klemensas <kdranseika@gmail.com>
2016-11-27 16:43:34 +0200

    Add pixel to coord and coord to pixel methods

--- src/app/game/services/map.service.ts
+++ src/app/game/services/map.service.ts
@@ -4,6 +4,7 @@ import { PlayerService } from './player.service';
 
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import * as seedrandom from 'seedrandom';
+import * as Big from 'big.js';
 
 @Injectable()
 export class MapService {
@@ -79,5 +80,83 @@ export class MapService {
       this.queuedPromise.push(resolve);
       this.socket.sendEvent('map', {});
     });
+  }
+
+  // Returns { x, y } pixels on the center of the coordinate
+  public coordToPixel(location, settings) {
+    const x = Big(location.x);
+    const y = Big(location.y);
+    const xOffset = y.mod(2).times(-0.5);
+    return {
+      x: xOffset.plus(x).times(settings.width),
+      y: y.minus(0.25).times(settings.aHeight)
+    };
+  }
+
+
+  public pixelToCoord(pos, settings) {
+    let coords = {
+      x: Big(pos.x).div(settings.width).plus(1).round(0, 0),
+      y: Big(pos.y).div(settings.aHeight).plus(1).round(0, 0)
+    };
+    const offset = {
+      x: Big(pos.x).mod(settings.width),
+      y: Big(pos.y).mod(settings.aHeight),
+    };
+    const type = +coords.y % 2 ? odd : even;
+    coords = type(coords);
+    const coordOffset = this.coordToPixel(coords, settings);
+    const realOffset = {
+      x: coordOffset.x.minus(pos.x).minus(settings.radius),
+      y: coordOffset.y.minus(pos.y).minus(settings.side),
+    };
+    // Adjust coord if near edge
+    if (realOffset.y.gt(-settings.hexHeight)) {
+      coords.y = coords.y.minus(1);
+      const parity = +coords.y % 2;
+      realOffset.y = realOffset.y.minus(settings.aHeight);
+      if (realOffset.x.lte(settings.radius.times(-1))) {
+        coords.x = parity ? coords.x.plus(1) : coords.x;
+        realOffset.x = realOffset.x.plus(settings.radius);
+      } else {
+        coords.x = parity ? coords.x : coords.x.minus(1);
+        realOffset.x = realOffset.x.minus(settings.radius);
+      }
+    }
+    return {
+      xCoord: coords.x,
+      yCoord: coords.y,
+      x: realOffset.x,
+      y: realOffset.y,
+      xPx: pos.x,
+      yPx: pos.y
+    }
+
+    function odd(coords) {
+      if (offset.y.lt(settings.hexHeight.minus(offset.x).times(settings.edgeGradient))) {
+        coords.x = coords.x.minus(1);
+        coords.y = coords.y.minus(1);
+      }
+      if (offset.y.lt(-settings.hexHeight.plus(offset.x).times(settings.edgeGradient))) {
+        coords.y = coords.y.minus(1);
+      }
+      return coords;
+    };
+
+    function even(coords) {
+      if (offset.x.gte(settings.radius)) {
+        if (offset.y.lt(settings.side.minus(offset.x).times(settings.edgeGradient))) {
+          coords.y = coords.y.minus(1);
+        }
+        return coords;
+      }
+
+      if (offset.y.lt(offset.x.times(settings.edgeGradient))) {
+        coords.y = coords.y.minus(1);
+      } else {
+        coords.x = coords.x.minus(1);
+      }
+      return coords;
+    }
   }
 }
