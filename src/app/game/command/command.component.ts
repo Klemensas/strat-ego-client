6ffef8be07f22a031b139f815d3a0414a6524b48
Klemensas <kdranseika@gmail.com>
2017-01-08 20:46:41 +0200

    Display troops to send in command component

--- src/app/game/command/command.component.ts
+++ src/app/game/command/command.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GameDataService } from '../../services/game-data.service'
+import { TownService, CommandService } from '../services/'
 
 @Component({
   selector: 'command',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./command.component.scss']
 })
 export class CommandComponent implements OnInit {
+  private townUnits;
+  private unitData;
+  private unitDataMap;
+  private target;
+  private unitsToSend;
 
-  constructor() { }
+  constructor(private townService: TownService, private commandService: CommandService, private gameData: GameDataService) {
+    this.target = commandService.targeting || [];
+    console.log(this.target);
+  }
 
   ngOnInit() {
+    this.gameData.data.activeWorld.subscribe(world => {
+      this.unitData = world.unitData;
+      this.unitDataMap = world.unitDataMap;
+      console.log(world)
+    });
+    this.townService.currentTown.subscribe(town => {
+      if (town) {
+        this.townUnits = town.units;
+      }
+    });
+  }
+
+  sendComand(isSupport) {
+    console.log(isSupport);
   }
 
 }
