56eefadc2947ff80fb66b6a626ea9229a923b22a
Klemensas <hello@klemensas.net>
2018-10-26 01:21:12 +0300

    feat: update town component

--- src/app/game/town/town.component.ts
+++ src/app/game/town/town.component.ts
@@ -1,6 +1,6 @@
 // TODO: refactor some town elements in to simple presentation components
 
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { getActiveWorld } from '../../reducers';
@@ -8,44 +8,25 @@ import { GameModuleState, getFullTown } from '../reducers';
 import { Rename, Build } from '../town/town.actions';
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'town',
   templateUrl: './town.component.html',
   styleUrls: ['./town.component.scss'],
 })
 
-export class TownComponent implements OnInit, OnDestroy {
-  private nameChange = '';
-  public town$ = this.store.select(getFullTown);
-  public worldData$ = this.store.select(getActiveWorld);
+export class TownComponent {
+  nameChange = '';
+  town$ = this.store.select(getFullTown);
+  worldData$ = this.store.select(getActiveWorld);
 
-  constructor(
-    private store: Store<GameModuleState>,
-  ) {}
+  constructor(private store: Store<GameModuleState>) {}
 
-  public ngOnInit() {
-    // Subscribe to town data updates
-    // this.store.select(getActiveTown).subscribe((town) => {
-    //   this.town = town as any;
-    // })
-    // this.townObserver = this.townService.currentTown.subscribe(update => {
-    //   console.log('hmm', update)
-    //   this.town = update;
-    // });
-  }
-
-  public rename(targetName, oldName) {
-    if (targetName.length > 3 && targetName !== oldName) {
-      this.store.dispatch(new Rename(targetName));
+  rename(name: string, oldName: string, town: number) {
+    if (name.length > 3 && name !== oldName) {
+      this.store.dispatch(new Rename({ name, town }));
     }
   }
 
-  public build(building) {
+  build(building) {
     this.store.dispatch(new Build(building));
-  }
-
-
-  ngOnDestroy() {
-    // this.townObserver.unsubscribe();
   }
 }
